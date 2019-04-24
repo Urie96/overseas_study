50665ed7894579a25c48df31239fb06639abe51a
Urie96 <1043629668@qq.com>
2019-04-24 11:20:28 +0800

    Initial commit

---
+++ src/main/java/me/urie/overseas_study/OverseasStudyApplication.java
@@ -0,0 +1,13 @@
+package me.urie.overseas_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverseasStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverseasStudyApplication.class, args);
+    }
+
+}
